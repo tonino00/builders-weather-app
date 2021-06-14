a6c10bcdd169e1961c9d23face4cfee91fedf232
tonino00 <antoninocgama@hotmail.com>
2021-06-13 23:45:37 -0300

    infoCard new feature card

--- builders/components/InfoCard/index.js
+++ builders/components/InfoCard/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import {Card, CardItem, Text} from 'native-base';
 
 const InfoCard = props => {
+  function convertKelvinToC(kelvin) {
+    return parseInt(kelvin - 273);
+  }
+
+  let tempMin = props.main.temp_min;
+  let tempMax = props.main.temp_max;
+  let humidity = props.main.humidity;
+  let wind = props.wind.speed;
+
   return (
     <Card>
       <Text
@@ -13,7 +22,12 @@ const InfoCard = props => {
         }}>
         Info do dia
       </Text>
-      <CardItem style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+      <CardItem
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          textAlign: 'center',
+        }}>
         <Text>Vento</Text>
         <Text>Umidade</Text>
         <Text>Temp.Máx</Text>
@@ -24,10 +38,10 @@ const InfoCard = props => {
           flexDirection: 'row',
           justifyContent: 'space-between',
         }}>
-        <Text>22 km/h</Text>
-        <Text>{props.location.base}</Text>
-        <Text>33 °c</Text>
-        <Text>25 °c</Text>
+        <Text>{wind}</Text>
+        <Text>{humidity} %</Text>
+        <Text>{convertKelvinToC(tempMax)} °c</Text>
+        <Text>{convertKelvinToC(tempMin)}°c</Text>
       </CardItem>
     </Card>
   );
