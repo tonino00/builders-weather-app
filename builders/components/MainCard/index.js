8436b6aedacc14158f78fbcd1e3019fdaf029814
tonino00 <antoninocgama@hotmail.com>
2021-06-13 23:46:55 -0300

    mainCard component informations

--- builders/components/MainCard/index.js
+++ builders/components/MainCard/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ImageBackground} from 'react-native';
 import {Card, CardItem, Text, Icon, Body} from 'native-base';
 
 const MainCard = props => {
   return (
     <Card>
-      <CardItem>
+      <CardItem style={{backgroundColor: '#c3c3c3'}}>
         <Text style={styles.text}>{props.title}</Text>
       </CardItem>
       <CardItem bordered>
-        <Body style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <View style={{alignItems: 'center', margin: 0}}>
-            <Icon
-              ios={props.ios}
-              android={props.android}
-              style={{fontSize: 40}}
-            />
-          </View>
-          <View
-            style={{
-              flexDirection: 'row',
-            }}>
-            <Text style={{fontSize: 30}}>{props.temperature}</Text>
-            <Text>°c</Text>
-          </View>
-        </Body>
+        <ImageBackground
+          source={props.backgrounImg}
+          style={{resizeMode: 'cover', flex: 1}}>
+          <Body style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{alignItems: 'center', margin: 0}}>
+              <Icon
+                ios={props.ios}
+                android={props.android}
+                style={{fontSize: 40}}
+              />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+              }}>
+              <Text style={{fontSize: 30}}>{props.temperature}</Text>
+              <Text>°c</Text>
+            </View>
+          </Body>
+        </ImageBackground>
       </CardItem>
     </Card>
   );
