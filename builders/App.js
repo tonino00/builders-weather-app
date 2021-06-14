887b01256803dd65f598c9b2e5481ab5116e6603
tonino00 <antoninocgama@hotmail.com>
2021-06-13 23:42:45 -0300

    new visual and ajust api

--- builders/App.js
+++ builders/App.js
@@ -1,29 +1,86 @@
-import {StatusBar} from 'native-base';
-import React, {useState, useEffect, Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import MainCard from './components/MainCard/index';
 import PanelCard from './components/PanelCard/index';
 import InfoCard from './components/InfoCard/index';
-import LoaderHeader from './components/Header/index';
-import {Container, Content} from 'native-base';
+import {
+  Container,
+  Content,
+  Header,
+  Left,
+  Button,
+  Icon,
+  Body,
+  Right,
+  Title,
+} from 'native-base';
+import {ActivityIndicator, View, Text} from 'react-native';
 
 const App = () => {
-  const [location, setLocation] = useState([]);
-  const getLocation = async () => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=-9.662308&lon=-35.7555122&appid=40a5f7559101e8312445dfae9a9b0db1`;
+  const [main, setMain] = useState([]);
+  const [wind, setWind] = useState([]);
+  const [sys, setSys] = useState([]);
+  const [othersInfo, othersInfoSet] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  const getData = async () => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=-9.66625&lon=-35.735193959&appid=072b36ecacac162b8d289d3275a8b93a`;
     const response = await fetch(url);
-    const respnseJson = await response.json();
-    setLocation(respnseJson);
+    const responseJson = await response.json();
+    setisLoading(false);
+    setMain(responseJson.main);
+    setWind(responseJson.wind);
+    setSys(responseJson.sys);
+    othersInfoSet(responseJson);
   };
 
   useEffect(() => {
-    getLocation();
+    setisLoading(false);
+    getData();
   }, []);
+
+  async function setData() {
+    await getData();
+  }
+
+  useEffect(() => {
+    setData();
+  }, []);
+
+  function convertKelvinToC(kelvin) {
+    return parseInt(kelvin - 273);
+  }
+
+  if (isLoading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: 100,
+          backgroundColor: '#FFFFFF',
+        }}>
+        <ActivityIndicator color="blue" size={36} />
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
 
   return (
     <Container>
-      <LoaderHeader />
-      <Content padder>
-        <PanelCard />
+      <Header>
+        <Left style={{flex: 1}}>
+          <Button transparent onPress={() => setData()}>
+            <Icon name="reload-outline" style={{fontSize: 20}} />
+          </Button>
+        </Left>
+        <Body>
+          <Title style={{marginLeft: 2, marginRight: 2}}>Clima App</Title>
+        </Body>
+        <Right style={{flex: 1}} />
+      </Header>
+      <Content>
+        <PanelCard main={main} sys={sys} othersInfo={othersInfo} />
         <MainCard
           title={'Manhã'}
           temperature={'26'}
@@ -32,17 +89,17 @@ const App = () => {
         />
         <MainCard
           title={'Tarde'}
-          temperature={'32'}
+          temperature={'33'}
           ios={'ios-partly-sunny-outline'}
           android={'md-partly-sunny-outline'}
         />
         <MainCard
           title={'Noite'}
-          temperature={'21'}
+          temperature={convertKelvinToC(main.temp_min)}
           ios={'ios-cloudy-night-outline'}
           android={'md-cloudy-night-outline'}
         />
-        <InfoCard location={location} />
+        <InfoCard main={main} wind={wind} />
       </Content>
     </Container>
   );
